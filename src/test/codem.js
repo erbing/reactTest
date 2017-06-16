9c9c26815fab707b56e7ffa0d4b72bfefd320915
zigzga <447587073@qq.com>
2017-06-16 11:16:53 +0800

    codem

---
+++ src/test/codem.js
@@ -0,0 +1,19 @@
+// 多行
+var n = parseInt(readline());
+var ans = 0;
+for(var i = 0;i < n; i++){
+    lines = readline().split(" ")
+    for(var j = 0;j < lines.length; j++){
+        ans += parseInt(lines[j]);
+    }
+    print(ans);
+}
+
+
+// 单行
+while(line=readline()){
+    var lines = line.split(" ");
+    var a = parseInt(lines[0]);
+    var b = parseInt(lines[1]);
+    print(a+b);
+}
