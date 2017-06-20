2d611e9a990ead960c8b1f793b1c9f106c7530d1
zigzga <447587073@qq.com>
2017-06-20 23:22:53 +0800

    http

---
+++ src/test/http.js
@@ -0,0 +1,23 @@
+
+http
+
+// 请求报文
+
+// 1、起始行   请求request    响应 response
+
+request :  get
+
+Response:  http 1.1
+
+
+// 请求header
+content-type
+
+content-length  
+
+
+// 主体   request  (null)   response (html content)
+
+ 
+
+ 
